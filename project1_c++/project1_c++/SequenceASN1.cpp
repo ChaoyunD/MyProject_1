929b67141e1d626f00fed2084f1191661114810e
dou <136921182@qq.com>
2018-08-02 10:40:39 +0800

    使用c++的类实现了编码和解码

---
+++ project1_c++/project1_c++/SequenceASN1.cpp
@@ -0,0 +1,120 @@
+﻿#include "SequenceASN1.h"
+
+SequenceASN1::SequenceASN1()
+{
+
+}
+
+int SequenceASN1::writeHeadNode(int iValue)//此函数或下面函数只调用一次
+{
+	DER_ItAsn1_WriteInteger(iValue, &m_header);
+	return 0;
+}
+
+int SequenceASN1::writeHeadNode(char * sValue, int len)
+{
+	DER_ITCAST_String_To_AnyBuf(&m_temp, (unsigned char *)sValue, len+1);
+	DER_ItAsn1_WritePrintableString(m_temp, &m_header);
+	return 0;
+}
+
+int SequenceASN1::writeNextNode(int iValue)
+{
+	if (m_header->next == NULL)
+	{
+		DER_ItAsn1_WriteInteger(iValue, &m_header->next);
+		m_next = m_header->next;
+	}
+	else
+	{
+		DER_ItAsn1_WriteInteger(iValue, &m_next->next);
+		m_next = m_next->next;
+	}
+	return 0;
+}
+
+int SequenceASN1::writeNextNode(char * sValue, int len)
+{
+	if (m_header->next == NULL)
+	{
+		EncodeChar(sValue, len + 1, &m_header->next);
+		m_next = m_header->next;
+	}
+	else
+	{
+		EncodeChar(sValue, len + 1, &m_next->next);
+		m_next = m_next->next;
+	}
+	return 0;
+}
+
+int SequenceASN1::readHeadNode(int & iValue)//此函数或下面函数只调用一次
+{
+	DER_ItAsn1_ReadInteger(m_temp,(ITCAST_UINT32 *)&m_header);
+	m_next = m_header->next;
+	return 0;
+}
+
+int SequenceASN1::readHeadNode(char * sValue)
+{
+	DER_ItAsn1_ReadPrintableString(m_header, &m_temp);
+	memcpy(sValue, m_temp->pData, m_temp->dataLen);
+	m_next = m_header->next;
+	DER_ITCAST_FreeQueue(m_temp);
+	return 0;
+}
+
+int SequenceASN1::readNextNode(int & iValue)
+{
+	DER_ItAsn1_ReadInteger(m_next, (ITCAST_UINT32 *)&iValue);
+	m_next = m_next->next;
+	return 0;
+}
+
+int SequenceASN1::readNextNode(char** sValue)//针对老师的内容修改了这里
+{
+	DER_ItAsn1_ReadPrintableString(m_next, &m_temp);
+	*sValue = (char *)malloc(m_temp->dataLen);
+	strcpy(*sValue, (char *)m_temp->pData);
+	//printf("%s\n", sValue);
+	m_next = m_next->next;
+	DER_ITCAST_FreeQueue(m_temp);
+	return 0;
+}
+
+int SequenceASN1::readNextNode(char * sValue)
+{
+	DER_ItAsn1_ReadPrintableString(m_next, &m_temp);
+	sValue = (char *)malloc(m_temp->dataLen);
+	strcpy(sValue, (char *)m_temp->pData);
+	//printf("%s\n", sValue);
+	m_next = m_next->next;
+	DER_ITCAST_FreeQueue(m_temp);
+	return 0;
+	return 0;
+}
+
+int SequenceASN1::packSequence(char ** outData, int & outLen)
+{
+	DER_ItAsn1_WriteSequence(m_header, &m_temp);
+	*outData = (char *)m_temp->pData;
+	outLen = m_temp->dataLen;
+	freeSequence(m_header);
+	return 0;
+}
+
+int SequenceASN1::unpackSequence(char * inData, int inLen)
+{
+	m_header = NULL;
+	m_next = NULL;
+	m_temp = NULL;
+	DER_ITCAST_String_To_AnyBuf(&m_temp,(unsigned char *)inData, inLen);
+	DER_ItAsn1_ReadSequence(m_temp, &m_header);
+	freeSequence(m_temp);
+	return 0;
+}
+
+void SequenceASN1::freeSequence(ITCAST_ANYBUF * node)
+{
+	DER_ITCAST_FreeQueue(node);
+}
---
+++ project1_c++/project1_c++/main.cpp
@@ -0,0 +1,53 @@
+﻿#include "BaseASN1.h"
+#include "ItcastLog.h"
+#include "SequenceASN1.h"
+typedef struct _Teacher
+{
+	char name[64];
+	int age;
+	char *p;
+	int plen;
+}Teacher;
+SequenceASN1 seq;
+int encodeTeacher(Teacher * p, char ** outData, int & outlen)//编码
+{
+	
+	seq.writeNextNode(p->age);
+	seq.writeNextNode(p->p, strlen(p->p));
+	seq.writeNextNode(p->plen);
+	seq.packSequence(outData, outlen);// 数据关联 - 整个结构体数据块,编码整个结构体
+	return 0;
+}
+int decodeTeacher(char * inData, int inLen, Teacher ** p)//解码
+{
+	seq.unpackSequence(inData, inLen);
+	Teacher* pt = (Teacher*)malloc(sizeof(Teacher));
+	seq.readHeadNode(pt->name);
+	seq.readNextNode(pt->age);
+	seq.readNextNode(&pt->p);
+	seq.readNextNode(pt->plen);
+	*p = pt;
+	return 0;
+}
+int main(void)
+{
+	Teacher tea;
+	strcpy(tea.name, "路飞");
+	tea.age = 20;
+	tea.p = (char*)malloc(100);
+	strcpy(tea.p, "我是要成为海贼王的男人");
+	tea.plen = strlen(tea.p);
+	//编码
+	char *outData=NULL;
+	int outlen;
+	encodeTeacher(&tea, &outData, outlen);
+	//解码
+	Teacher* pt=NULL;
+	decodeTeacher(outData, outlen, &pt);
+	printf("name:	%s\n", pt->name);
+	printf("age:	%d\n", pt->age);
+	printf("p:	%s\n", pt->p);
+	printf("plen:	%d\n", pt->plen);
+	system("pause");
+	return 0;
+}
